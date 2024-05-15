f990a977cf2cd9aa0e8a79231cf23ae90ebc07d0
majdba123 <137396563+majdba123@users.noreply.github.com>
2024-05-15 14:32:20 +0300

    1
    1

--- booking_bus/routes/api.php
+++ booking_bus/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[UserApiController::class,'register']);
+Route::post('login',[UserApiController::class,'login']);
+Route::post('logout',[UserApiController::class,'logout'])->middleware('auth:sanctum');
