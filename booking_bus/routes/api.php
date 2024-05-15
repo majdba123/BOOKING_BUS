e32b58895c4a80e71108c402d8a80f450ac0cca8
majdba123 <137396563+majdba123@users.noreply.github.com>
2024-05-15 14:47:54 +0300

    controller
    create auth controller

--- booking_bus/routes/api.php
+++ booking_bus/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserApiController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
