6d83ac9da990bb975ba3c7d001dfb0a8b6d42905
majdba123 <137396563+majdba123@users.noreply.github.com>
2024-06-23 14:29:28 +0300

    path
    register comany and driver and all mession of path

--- booking_bus/app/Http/Controllers/PathController.php
+++ booking_bus/app/Http/Controllers/PathController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Path;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 class PathController extends Controller
 {
     /**
@@ -12,7 +14,10 @@ class PathController extends Controller
      */
     public function index()
     {
-        //
+        $company=Auth::user()->Company->id;
+
+        $paths = Path::where('company_id', $company )->get();
+        return response()->json($paths, 200);
     }
 
     /**
@@ -28,7 +33,23 @@ class PathController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'from' => 'required|string',
+            'to' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->first();
+            return response()->json(['error' => $errors], 422);
+        }
+        $path = new Path();
+        $path->from = $request->input('from');
+        $path->to = $request->input('to');
+        $path->company_id = Auth::user()->id;
+        $path->save();
+
+        return response()->json([
+            'message' => 'path Created ',
+        ]);
     }
 
     /**
@@ -50,16 +71,40 @@ class PathController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Path $path)
+    public function update(Request $request,  $pathh)
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'from' => 'sometimes|string',
+            'to' => 'sometimes|string',
 
+        ]);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->first();
+            return response()->json(['error' => $errors], 422);
+        }
+        $path=Path::findOrfail($pathh);
+        if ($path->company_id!== auth()->user()->id) {
+            return response()->json(['error' => 'you are not owner to update path'], 403);
+        }
+        $path->from = $request->input('from') ?? $path->from;
+        $path->to = $request->input('to') ?? $path->to;
+        $path->save();
+        return response()->json([
+            'message' => 'path updated',
+        ]);
+    }
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Path $path)
+    public function destroy($path)
     {
-        //
+        $path=Path::findOrfail($path);
+        if ($path->company_id!== auth()->user()->id) {
+            return response()->json(['error' => 'you are not owner to update path'], 403);
+        }
+        $path->delete();
+        return response()->json([
+            'message' => 'path deleted  ',
+        ]);
     }
 }
--- booking_bus/routes/api.php
+++ booking_bus/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserApiController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\PathController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +26,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register',[UserApiController::class,'register']);
 Route::post('login',[UserApiController::class,'login']);
 Route::post('logout',[UserApiController::class,'logout'])->middleware('auth:sanctum');
+
+
+Route::post('register/company',[CompanyController::class,'register']);
+
+
+
+
+
+
+Route::group(['prefix' => 'company' , 'middleware' => ['company','auth:sanctum']], function () {
+
+    Route::post('register/driver',[CompanyController::class,'register_driver']);
+
+    Route::get('/all_path', [PathController::class, 'index']);
+    Route::post('/path_store', [PathController::class, 'store']);
+    Route::put('/path_update/{id}', [PathController::class, 'update']);
+    Route::delete('/path_delete/{id}', [PathController::class, 'destroy']);
+});
