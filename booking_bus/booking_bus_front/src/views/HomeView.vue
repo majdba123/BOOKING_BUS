3691106c012892e1bfb96f624be60fe322b2b5d6
Mohmadkahal22 <mohmadalkahal@gmail.com>
2024-06-28 17:11:42 -0500

    test
    test

--- booking_bus/booking_bus_front/src/views/HomeView.vue
+++ booking_bus/booking_bus_front/src/views/HomeView.vue
@@ -1,52 +1,53 @@
 <template>
-    <form action="action_page.php">
-        <div class="container">
-            <h1>Register</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr />
-            <label for="name"><b>name</b></label>
-            <input
-                type="text"
-                placeholder="Enter name"
-                name="name"
-                id="name"
-                required
-                v-model="name"
-            />
-            <label for="email"><b>Email</b></label>
-            <input
-                type="text"
-                placeholder="Enter Email"
-                name="email"
-                id="email"
-                required
-                v-model="email"
-            />
+    <div class="container">
+        <h1>Register</h1>
+        <p>Please fill in this form to create an account.</p>
+        <hr />
+        <label for="name"><b>name</b></label>
+        <input
+            type="text"
+            placeholder="Enter name"
+            name="name"
+            id="name"
+            required
+            v-model="name"
+        />
+        <label for="email"><b>Email</b></label>
+        <input
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            id="email"
+            required
+            v-model="email"
+        />
 
-            <label for="psw"><b>Password</b></label>
-            <input
-                type="password"
-                placeholder="Enter Password"
-                name="psw"
-                id="psw"
-                required
-                v-model="password"
-            />
+        <label for="psw"><b>Password</b></label>
+        <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            id="psw"
+            required
+            v-model="password"
+        />
 
-            <hr />
+        <hr />
 
-            <p>
-                By creating an account you agree to our
-                <a href="#">Terms & Privacy</a>.
-            </p>
+        <p>
+            By creating an account you agree to our
+            <a href="#">Terms & Privacy</a>.
+        </p>
 
-            <button @click="register()" class="registerbtn">Register</button>
-        </div>
+        <button @click="register()" class="registerbtn">Register</button>
+    </div>
 
-        <div class="container signin">
-            <p>Already have an account? <a href="#">Sign in</a>.</p>
-        </div>
-    </form>
+    <div class="container signin">
+        <p>
+            Already have an account?
+            <router-link to="abour">Sign in.</router-link>
+        </p>
+    </div>
 </template>
 
 <script>
@@ -74,8 +75,6 @@ export default {
                 })
                 .catch(function (error) {
                     console.log(error);
-
-                    window.alert(error.response.message);
                 });
         },
     },
