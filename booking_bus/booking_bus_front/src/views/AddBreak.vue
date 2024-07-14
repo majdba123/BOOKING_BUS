19b3326c068f853133aadc03e27b1468f89b3a5c
mohamaddefalla <hamoudadefalla154@gmail.com>
2024-07-14 20:01:26 +0300

    goverment break
    add break

--- booking_bus/booking_bus_front/src/views/AddBreak.vue
+++ booking_bus/booking_bus_front/src/views/AddBreak.vue
@@ -14,6 +14,31 @@
                             v-model="name"
                         />
                     </div>
+                    <div class="dropdown">
+                        <button @click="toggleDropdown" class="dropbtn">
+                            Select Government
+                        </button>
+                        <div
+                            :class="{
+                                'dropdown-content': true,
+                                show: dropdownOpen,
+                            }"
+                        >
+                            <input
+                                type="text"
+                                placeholder="Search.."
+                                @keyup="filterFunction"
+                                ref="myInput"
+                            />
+                            <a
+                                v-for="(link, index) in filteredList"
+                                :key="index"
+                                @click="selectGovernment(link)"
+                            >
+                                {{ link.name }}
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <button class="Button" @click="AddBreak">ADD</button>
             </div>
@@ -32,15 +57,55 @@ export default {
     data() {
         return {
             name: "",
+            dropdownOpen: false,
+            links: [],
+            filteredList: [],
+            selectedGovernment: null,
         };
     },
+
     methods: {
+        toggleDropdown() {
+            this.dropdownOpen = !this.dropdownOpen;
+            this.filteredList = this.links;
+        },
+        filterFunction() {
+            const input = this.$refs.myInput.value.toUpperCase();
+            this.filteredList = this.links.filter((link) =>
+                link.name.toUpperCase().includes(input)
+            );
+        },
+        selectGovernment(link) {
+            this.selectedGovernment = link.id;
+            this.dropdownOpen = false;
+        },
+        fetchGovernment() {
+            const access_token = window.localStorage.getItem("access_token");
+
+            axios({
+                method: "get",
+                url: "http://127.0.0.1:8000/api/admin/all_government",
+                headers: { Authorization: `Bearer ${access_token}` },
+            })
+                .then((response) => {
+                    this.links = response.data;
+                    console.log(this.links);
+                })
+                .catch((error) => {
+                    window.alert("Error getting Government");
+                    console.error(error);
+                });
+        },
         AddBreak() {
+            if (!this.selectedGovernment) {
+                window.alert("Please select a government.");
+                return;
+            }
             const token = window.localStorage.getItem("access_token");
 
             axios({
                 method: "post",
-                url: "127.0.0.1:8000/api/admin/store_breaks/" + "5",
+                url: `http://127.0.0.1:8000/api/admin/store_breaks/${this.selectedGovernment}`,
                 data: {
                     name: this.name,
                 },
@@ -56,10 +121,13 @@ export default {
                 });
         },
     },
+    mounted() {
+        this.fetchGovernment();
+    },
 };
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .main-content {
     display: flex;
     width: 100%;
@@ -81,13 +149,14 @@ export default {
 }
 .box {
     display: flex;
+    flex-direction: column;
+    align-items: center;
     margin: 30px;
     padding: 20px;
     background-color: white;
     border-radius: 15px;
     text-align: center;
     font-size: 20px;
-    justify-content: center;
 }
 .email span {
     color: black;
@@ -116,5 +185,51 @@ input:focus {
     padding: 10px 40px;
     border-radius: 15px;
     border: 1px solid gray;
+}
+
+.dropbtn {
+    background-color: #04aa6d;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+    border-radius: 20px;
+}
+
+.dropbtn:hover,
+.dropbtn:focus {
+    background-color: #3e8e41;
+}
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+    margin-top: 15px;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f6f6f6;
+    min-width: 230px;
+    overflow: auto;
+    border: 1px solid #ddd;
+    z-index: 1;
+}
+
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown-content a:hover {
+    background-color: #ddd;
+}
+
+.show {
+    display: block;
 }
 </style>
