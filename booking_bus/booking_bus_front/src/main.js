f2836099e1562565b80966d9c6cf31e1e5fb7a45
mohamaddefalla <hamoudadefalla154@gmail.com>
2024-07-30 02:51:49 +0300

    main
    path

--- booking_bus/booking_bus_front/src/main.js
+++ booking_bus/booking_bus_front/src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from "vue";
-//import BookingLogin from "./views/BookingLogin.vue"; // Adjusted import path
+import BookingLogin from "./views/BookingLogin.vue";
 import router from "./router";
 import store from "./store";
-import App from "./App.vue";
 
-const app = createApp(App);
+const app = createApp(BookingLogin);
 
 app.use(store).use(router).mount("#app");
