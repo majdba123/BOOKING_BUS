971b6e1e79e2b9129e6f10710dfcbb4066300ef0
mohamaddefalla <hamoudadefalla154@gmail.com>
2024-07-30 01:26:22 +0300

    complete
    test

--- booking_bus/booking_bus_front/src/components/NavBar.vue
+++ booking_bus/booking_bus_front/src/components/NavBar.vue
@@ -1,9 +1,4 @@
 <template>
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    />
-
     <div class="main-content">
         <div class="menu">
             <ul>
@@ -144,7 +139,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 * {
     padding: 0;
     margin: 0;
--- booking_bus/booking_bus_front/src/main.js
+++ booking_bus/booking_bus_front/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import BookingLogin from "./views/BookingLogin.vue"; // Adjusted import path
+import BookingLogin from "./views/BookingLogin.vue";
 import router from "./router";
 import store from "./store";
 
