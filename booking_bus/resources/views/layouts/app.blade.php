4afaec07792fda0c68dd4a98f0689eda4a5c23cf
majdba123 <137396563+majdba123@users.noreply.github.com>
2024-08-02 19:05:26 +0300

    installl pusher
    instaall

--- booking_bus/resources/views/layouts/app.blade.php
+++ booking_bus/resources/views/layouts/app.blade.php
@@ -76,5 +76,21 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+    <script>
+
+      // Enable pusher logging - don't include this in production
+      Pusher.logToConsole = true;
+
+      var pusher = new Pusher('7342c00647f26084d14f', {
+        cluster: 'ap2'
+      });
+
+      var channel = pusher.subscribe('my-channel');
+      channel.bind('my-event', function(data) {
+        alert(JSON.stringify(data));
+      });
+    </script>
 </body>
 </html>
