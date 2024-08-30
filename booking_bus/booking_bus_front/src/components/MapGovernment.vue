c6f21cae51ccf6fff1ac84592f47e37baae0b4a3
Mohmadkahal22 <mohmadalkahal@gmail.com>
2024-08-30 15:48:08 -0500

    update
    update key api

--- booking_bus/booking_bus_front/src/components/MapGovernment.vue
+++ booking_bus/booking_bus_front/src/components/MapGovernment.vue
@@ -27,7 +27,7 @@ export default {
     methods: {
         loadGoogleMapsScript() {
             const script = document.createElement("script");
-            script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAeLUpyozCjrCIxNBNmwVfCERYrHZh3MbU&libraries=places&callback=initAutocomplete`;
+            script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDhQD0CiLwIDWv0AR7noQSjX1w0SU6pHGo&libraries=places&callback=initAutocomplete`;
             script.async = true;
             script.defer = true;
             document.head.appendChild(script);
