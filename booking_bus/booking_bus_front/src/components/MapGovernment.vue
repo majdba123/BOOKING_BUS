b59a046a7d3fca9e5800e5e87c455dba9348aeaf
Majd Bayer <137396563+majdba123@users.noreply.github.com>
2026-09-06 07:16:27 +0300

    security: use centralized Google Maps loader

--- booking_bus/booking_bus_front/src/components/MapGovernment.vue
+++ booking_bus/booking_bus_front/src/components/MapGovernment.vue
@@ -11,6 +11,8 @@
 /* global google */
 import store from "@/store";
 import { useToast } from "vue-toastification";
+import { loadGoogleMaps } from "@/utils/googleMapsLoader";
+
 export default {
     data() {
         return {
@@ -19,108 +21,92 @@ export default {
             toast: null,
         };
     },
-    mounted() {
+    async mounted() {
         this.toast = useToast();
-        this.loadGoogleMapsScript();
+
+        try {
+            await loadGoogleMaps();
+            this.initAutocomplete();
+            this.toast.info("Please search for a Government.");
+        } catch (error) {
+            console.error("Unable to initialize Google Maps:", error);
+            this.toast.error("Unable to load Google Maps.");
+        }
     },
     methods: {
-        loadGoogleMapsScript() {
-            const script = document.createElement("script");
-            script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDd9RLeRSNjmt1AIx22VeWqwbxYh3myC44&libraries=places&callback=initAutocomplete`;
-            script.async = true;
-            script.defer = true;
-            document.head.appendChild(script);
-
-            script.onload = () => {
-                this.initAutocomplete();
-                this.toast.info("Please search for a Government.");
-            };
-
-            script.onerror = () => {
-                // console.error("Failed to load Google Maps script");
-                setTimeout(this.loadGoogleMapsScript, 3000);
-            };
-        },
         initAutocomplete() {
-            if (typeof google !== "undefined") {
-                this.map = new google.maps.Map(document.getElementById("map"), {
-                    center: { lat: 34.8021, lng: 38.9968 },
-                    zoom: 7,
-                    mapTypeId: "roadmap",
-                });
+            if (typeof google === "undefined") {
+                return;
+            }
 
-                const input = document.getElementById("pac-input");
-                const searchBox = new google.maps.places.SearchBox(input);
-                this.infoWindow = new google.maps.InfoWindow();
+            this.map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: 34.8021, lng: 38.9968 },
+                zoom: 7,
+                mapTypeId: "roadmap",
+            });
 
-                this.map.addListener("bounds_changed", () => {
-                    searchBox.setBounds(this.map.getBounds());
-                });
+            const input = document.getElementById("pac-input");
+            const searchBox = new google.maps.places.SearchBox(input);
+            this.infoWindow = new google.maps.InfoWindow();
 
-                let markers = [];
+            this.map.addListener("bounds_changed", () => {
+                searchBox.setBounds(this.map.getBounds());
+            });
 
-                searchBox.addListener("places_changed", () => {
-                    const places = searchBox.getPlaces();
+            let markers = [];
 
-                    if (places.length === 0) {
+            searchBox.addListener("places_changed", () => {
+                const places = searchBox.getPlaces();
+
+                if (places.length === 0) {
+                    return;
+                }
+
+                markers.forEach((marker) => marker.setMap(null));
+                markers = [];
+
+                const bounds = new google.maps.LatLngBounds();
+
+                places.forEach((place) => {
+                    if (!place.geometry || !place.geometry.location) {
                         return;
                     }
 
-                    markers.forEach((marker) => {
-                        marker.setMap(null);
+                    const marker = new google.maps.Marker({
+                        map: this.map,
+                        title: place.name,
+                        position: place.geometry.location,
+                        icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
                     });
-                    markers = [];
 
-                    const bounds = new google.maps.LatLngBounds();
+                    marker.addListener("click", () => {
+                        const contentString = `
+                            <div>
+                                <strong>${place.name}</strong>
+                                <br>
+                                تم اختيار المنطقة
+                            </div>
+                        `;
+                        this.infoWindow.setContent(contentString);
+                        this.infoWindow.open(this.map, marker);
 
-                    places.forEach((place) => {
-                        if (!place.geometry || !place.geometry.location) {
-                            // console.log("Returned place contains no geometry");
-                            return;
-                        }
-
-                        const marker = new google.maps.Marker({
-                            map: this.map,
-                            title: place.name,
-                            position: place.geometry.location,
-                            icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
-                        });
-
-                        marker.addListener("click", () => {
-                            const contentString = `
-                                <div>
-                                    <strong>${place.name}</strong>
-                                    <br>
-                                    تم اختيار المنطقة
-                                </div>
-                            `;
-                            this.infoWindow.setContent(contentString);
-                            this.infoWindow.open(this.map, marker);
-
-                            store.state.placeName = place.name;
-                            store.state.lat = place.geometry.location.lat();
-                            store.state.lng = place.geometry.location.lng();
-                            // console.log(store.state.placeName);
-                            // console.log(store.state.lat);
-                            // console.log(store.state.lng);
-                        });
-
-                        markers.push(marker);
-
-                        if (place.geometry.viewport) {
-                            bounds.union(place.geometry.viewport);
-                        } else {
-                            bounds.extend(place.geometry.location);
-                        }
+                        store.state.placeName = place.name;
+                        store.state.lat = place.geometry.location.lat();
+                        store.state.lng = place.geometry.location.lng();
                     });
-                    this.map.fitBounds(bounds);
 
-                    this.toast.success("Please select the region(Click).");
+                    markers.push(marker);
+
+                    if (place.geometry.viewport) {
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
                 });
-            } else {
-                // console.error("Google Maps not loaded yet");
-                setTimeout(this.initAutocomplete, 3000);
-            }
+
+                this.map.fitBounds(bounds);
+                this.toast.success("Please select the region(Click).");
+            });
         },
     },
 };
@@ -147,7 +133,7 @@ export default {
     top: 10px;
     left: 50%;
     transform: translateX(-50%);
-    z-index: 1000; /* لضمان ظهورها فوق الخريطة */
+    z-index: 1000;
     background-color: white;
     border: 1px solid #ccc;
     border-radius: 3px;
